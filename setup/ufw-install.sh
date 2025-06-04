3f8ed775340743848b5bdc551a0688858708bb05
Ty <100667720+Xros3x@users.noreply.github.com>
2025-06-04 06:21:50 -0500

    Update ufw-install.sh

--- setup/ufw-install.sh
+++ setup/ufw-install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+# Update system
 sudo apt update
+
+# Install UFW
 sudo apt install ufw -y
+
+# Set default rules
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
+
+# Allow SSH before enabling firewall
 sudo ufw allow ssh
+
+# Enable UFW
 sudo ufw enable
+
+# Show status
 sudo ufw status verbose
